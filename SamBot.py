28e47f68678ac1bc91a2fb75923b329c6c038a86
Edward Clifford <edward.clifford@monomoy.edu>
2019-02-28 09:46:01 -0500

    Added hello function to ping user and welcome message

--- SamBot.py
+++ SamBot.py
@@ -11,15 +11,34 @@ async def on_ready():
     print('Logged in as {0.user}'.format(client))
 
 @client.event
+async def on_member_join(member):
+    """
+    Welcome message for new members joining the server
+    """
+    server = member.server
+    fmt = "Welcome {0.name} to {1.server}!"
+    await client.send_message(server, fmt.format(member, server))
+
+@client.event
 async def on_message(message):
+    """
+    Bot command function
+    """
+    #Bot will not respond to itself
     if message.author == client.user:
         return
 
-    if message.content.startswith('$help'):
+    #Help message
+    if message.content.startswith("$help"):
         await message.channel.send("""Hi, I'm SamBot
 Prefix your commands with a '$'
 Check out some of the cool things I can do!
 
 1. Look pretty""")
+
+    if message.content.startswith("$hello"):
+        await message.channel.send("Hello {0.author.mention}".format(message))
+
+
 
 client.run(discordToken.getToken())
