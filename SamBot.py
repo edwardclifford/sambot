346c38202f423faaa5691fdb4ceae99055d34b8f
Edward Clifford <edward.clifford@monomoy.edu>
2019-02-28 20:18:54 -0500

    Downgraded to Discord.py 3.6 library, using NSSM to host on pc

--- SamBot.py
+++ SamBot.py
@@ -1,5 +1,4 @@
 #Ted Clifford (c) 2.26.2019
-#Ian Kogut (c) 2.28.2019
 
 import discord
 import discordToken
@@ -22,27 +21,25 @@ async def on_member_join(member):
 
 @client.event
 async def on_message(message):
-    """
-    Bot command function
-    """
-    #Bot will not respond to itself
+    #The bot should not reply to itself
     if message.author == client.user:
         return
 
     #Help message
     if message.content.startswith("$help"):
-        await message.channel.send("""Hi, I'm SamBot
+        await client.send_message(message.channel, """Hi, I'm SamBot
 Prefix your commands with a '$'
 Check out some of the cool things I can do!
 
 1. Look pretty
 2. Be an Absolute UNIT """)
 
+    #Pings the messager
     if message.content.startswith("$hello"):
-        await message.channel.send("Hello {0.author.mention}".format(message))
+        await client.send_message(message.channel, "Hello {0.author.mention}".format(message))
 
-    if message.content.startwith("$awaken"):
-        await message.channel.send(""" YOU HAVE AWAKENED
+    if message.content.startswith("$awaken"):
+        await client.send_message(message.channel, """ YOU HAVE AWAKENED
 THE GREAT AND POWERFUL ME, howz u?""")
 
 client.run(discordToken.getToken())
